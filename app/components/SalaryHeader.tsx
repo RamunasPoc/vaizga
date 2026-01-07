aca2e70641b9dd6c230fe210918d6e0b1fba23bd
RamunasPoc <watusiaii@gmail.com>
2026-01-07 21:21:35 +0200

    3.4

--- app/components/SalaryHeader.tsx
+++ app/components/SalaryHeader.tsx
@@ -6,16 +6,6 @@ export default function SalaryHeader({ total }: { total: number }) {
   return (
     <div className="bg-linear-to-r from-green-500 to-green-700 rounded-xl p-6 sm:p-8 text-center sm:text-left relative flex flex-col sm:flex-row items-center sm:items-center gap-4 sm:gap-8 shadow-lg">
       
-      {/* Logotipas */}
-      <div className="w-24 h-24 sm:w-36 sm:h-36 shrink-0">
-        <Image
-          src="/logoss.png"
-          alt="Logo"
-          width={150}
-          height={150}
-          className="w-full h-full object-contain"
-        />
-      </div>
 
       {/* Tekstas */}
       <div className="flex flex-col items-center sm:items-start">
